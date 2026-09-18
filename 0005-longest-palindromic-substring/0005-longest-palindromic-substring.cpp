4a1cb70e680029925812fe90cd00397ba2a8a31e
Akshat Mishra <akshat.mishra2525@gmail.com>
2026-09-18 13:35:18 +0530

    Time: 11 ms (57.63%), Space: 18.4 MB (38.01%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
+    string gen_from_centre(string &s, int l, int r) {
 
-    int gen_from_centre(string &s){
-
-        while(l >= 0 && r < n && s[l] == s[r]){
-            l++;
-            r--;
+        while(l >= 0 && r < s.size() && s[l] == s[r]){
+            l--;
+            r++;
         }
+
+        return s.substr(l+1, r-l-1);
     }
 
     string longestPalindrome(string s) {
         
         int n = s.size();
+
+        string ans = "";
+
+        for(int i=0; i<n; i++){
+
+            string odd = gen_from_centre(s, i , i);
+            string even = gen_from_centre(s, i, i+1);
+
+            if(odd.size() > ans.size()){
+                ans = odd;
+            }
+
+            if(even.size() > ans.size()){
+                ans = even;
+            }
+        }
+
+        return ans;
         
     }
 };
