604a9b8327fe0977383dbb7aa1e3d23d3e6ebc78
Akshat Mishra <akshat.mishra2525@gmail.com>
2026-09-19 14:47:24 +0530

    Time: 0 ms (100%), Space: 8.3 MB (92.23%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+
+        int n = s.size();
+        string ans(n, ' ');
+
+        int mf = 0;
+        int maxi = 0;
+
+        vector<int> freq(26, 0);
+
+        for (char ch : s) {
+
+            int x = ch - 'a';
+
+            freq[x]++;
+
+            if (freq[x] > mf) {
+
+                maxi = x;
+                mf = freq[x];
+            }
+        }
+
+        if (mf > (n + 1) / 2)
+            return "";
+
+        int idx = 0;
+
+        while (freq[maxi] > 0) {
+
+            if (idx >= n)
+                idx = 1;
+
+            ans[idx] = 'a' + maxi;
+            freq[maxi]--;
+
+            idx += 2;
+        }
+
+        for (char ch : s) {
+
+            int x = ch - 'a';
+
+            while (freq[x] > 0) {
+
+                if (idx >= n)
+                    idx = 1;
+
+                ans[idx] = 'a' + x;
+                freq[x]--;
+
+                idx += 2;
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
